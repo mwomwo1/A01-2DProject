519acfd46921cb6f487375145fd6e30939d8d2f2
LeeHwangChoong <lyd8224@gmail.com>
2024-05-16 21:02:46 +0900

    각각 스크립트 설명

--- A01_Chapter3_2D/Assets/Scripts/Item/ItemSpawn.cs
+++ A01_Chapter3_2D/Assets/Scripts/Item/ItemSpawn.cs
@@ -2,5 +2,6 @@
 
 public class ItemSpawn : MonoBehaviour
 {
-
+    //아이템이 특정 시간대에 세가지중에 랜덤으로 하나 생성되게 하기
+    //혹은 적을 몇마리이상 잡을때마다 등장하게 하기
 }
--- A01_Chapter3_2D/Assets/Scripts/Item/ShieldItem.cs
+++ A01_Chapter3_2D/Assets/Scripts/Item/ShieldItem.cs
@@ -4,6 +4,6 @@ public class ShieldItem : ItemController
 {
     public override void ItemUse()
     {
-
+        //플레이어가 특정 시간동안 무적이 됨.
     }
 }
