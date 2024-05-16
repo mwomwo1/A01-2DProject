a05ae29d5bfa44cde639b42602de68cc202bdc52
LeeHwangChoong <lyd8224@gmail.com>
2024-05-17 00:18:09 +0900

    Item Spawn
    랜덤한 시간에 랜덤한 위치에서 랜덤한 아이템을 생성

--- A01_Chapter3_2D/Assets/Scripts/Item/ItemSpawn.cs
+++ A01_Chapter3_2D/Assets/Scripts/Item/ItemSpawn.cs
@@ -1,7 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ItemSpawn : MonoBehaviour
 {
-    //아이템이 특정 시간대에 세가지중에 랜덤으로 하나 생성되게 하기
-    //혹은 적을 몇마리이상 잡을때마다 등장하게 하기
+    //아이템이 특정 시간대에 세가지중에 랜덤으로 하나 생성되게 하기    
+
+    public GameObject[] itemPrefabs;     
+
+    private void Start()
+    {
+        // 최초 아이템 생성 스타트
+        StartCoroutine(SpawnRandomItem());
+    }
+
+    private IEnumerator SpawnRandomItem()
+    {
+        while (true)
+        {
+            // 랜덤한 시간 대기
+            float spawnInterval = Random.Range(15f, 40f);
+            yield return new WaitForSeconds(spawnInterval);
+
+            // 랜덤한 위치에 랜덤한 아이템을 생성
+            float randomX = Random.Range(-2.5f, 2.5f);
+            Vector2 spawnPosition = new Vector3(randomX, 4.7f);
+            int randomIndex = Random.Range(0, itemPrefabs.Length);
+            GameObject newItem = Instantiate(itemPrefabs[randomIndex], spawnPosition, Quaternion.identity);
+            
+            Destroy(newItem, 7f); // 7초 후에 아이템 삭제
+        }
+    }
 }
--- A01_Chapter3_2D/Assets/Scripts/Item/PowerUpItem.cs
+++ A01_Chapter3_2D/Assets/Scripts/Item/PowerUpItem.cs
@@ -6,7 +6,7 @@ public class PowerUpItem : ItemController
 {
     public override void ItemUse()
     {
-        //플레이어와 충돌하면 플레이어의 미사일의 속도가 증가 혹은 갯수가 증가 혹은 범위가 증가 
-        //특정시간이 지나면 다시 원래대로 돌아오게
+        //플레이어의 미사일의 속도가 증가 혹은 갯수가 증가 혹은 범위가 증가 
+        //특정시간이 지나면 다시 원래대로 돌아옴
     }
 }
