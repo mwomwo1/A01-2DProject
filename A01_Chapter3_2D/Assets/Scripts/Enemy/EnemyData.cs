562b879ee7b8c080bb14607b6e84524541cc100a
Kyr001 <cuppastorm@gmail.com>
2024-05-17 16:40:09 +0900

    Enemy 리소스 추가 및 코드 수정

--- A01_Chapter3_2D/Assets/Scripts/Enemy/EnemyData.cs
+++ A01_Chapter3_2D/Assets/Scripts/Enemy/EnemyData.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyData : MonoBehaviour
+public class EnemyData // : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //적 데이터
+    public int Hp { get; set; } // 몇 번 피격하면 파괴되는지 1~3
+    public float Speed { get; set; } // 이동 속도
 
-    // Update is called once per frame
-    void Update()
+
+    public EnemyData(int hp, float speed)
     {
-        
+        Hp = hp;
+        Speed = speed;
     }
 }
---
+++ A01_Chapter3_2D/Assets/Scripts/Enemy/NormalEnemy.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+
+public class NormalEnemy : MonoBehaviour
+{
+    public GameObject normalEnemy;
+    public EnemyData enemy = new(1, 0.01f);
+
+
+    //GameManager로 이동할 코드
+    void Start()
+    {
+        //InvokeRepeating("MakeEnemy", 0f, 3f);
+        MakeEnemy();
+    }
+
+    void Update()
+    {
+        MoveEnemy(enemy);
+    }
+
+    private void MakeEnemy()
+    {
+        // 일반 타입 enemy를 화면 상단에 랜덤 생성(배치)
+        //Instantiate(normalEnemy);
+
+        Debug.Log("화면 상단에 적 랜덤 생성");
+
+        float x = Random.Range(-3.0f, 3.0f);
+        float y = 5.0f;
+        transform.position = new Vector3(x, y);
+    }
+
+    private void MoveEnemy(EnemyData enemy)
+    {
+        // 살아있는 적 기본 이동(하강)
+
+        if (enemy.Hp > 0)
+        {
+            transform.position += Vector3.down * enemy.Speed;
+            if (transform.position.y < -4.0f)
+            {
+                Debug.Log("화면 최하단에 도달한 적 오브젝트를 파괴합니다.");
+                Destroy(normalEnemy, 3.0f);
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        // 총알 생성 함수 호출하기
+    }
+
+    //충돌 감지
+
+    private void DestoryEnemy() { }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            // 살아 있을 때 player의 bullet에 맞으면
+            // Hp가 줄고 player의 bullet을 파괴한다
+
+            if (enemy.Hp > 0)
+            {
+                enemy.Hp -= 1;
+                Destroy(collision.gameObject);
+            }
+            else if (enemy.Hp == 0)
+            {
+                Destroy(normalEnemy, 3.0f);
+                //GameManager.Instance.AddScore();
+            }
+        }
+    }
+}
